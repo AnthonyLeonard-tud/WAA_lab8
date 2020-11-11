af5270432362bc4e6d8ed9b4913cb284b5ddcaf5
aleonard_d20125047 <d20125047@mydit.ie>
2020-11-11 21:03:00 +0000

    put delete and get changes - need to test before submitting

--- webroot/articles-api.js
+++ webroot/articles-api.js
@@ -43,28 +43,58 @@ app.post('/articles', (req, res) => {
 // article routes
 app.get('/article/:articleID', (req, res) => {
     const articleID = req.params.articleID;
-    for (let article of Articles) {
-        if (article.articleID === articleID) {
-            res.json(article);
-            return;
-        }
-    }
-    res.status(404).send('article not found');
+
+    MongoClient.connect(url, function(err, client) {
+        const db = client.db(dbName);
+        const collection = db.collection('article');
+        const query = { "articleID": articleID };
+        collection.findOne(query, function(err, doc) {
+            if (err) {
+                return res.status(404).send('error');
+            } else if (!doc) {
+                return res.status(404).send('article not found');
+            } else
+            //console.log(doc);
+                res.status(200).json(doc);
+        });
+        client.close();
+    });
 });
 
 app.delete('/article/:articleID', (req, res) => {
     const articleID = req.params.articleID;
 
-    //console.log(articleID);
-
-    Articles = Articles.filter(i => {
-        if (i.articleID !== articleID) {
-            return true;
-        }
-        return false;
+    MongoClient.connect(url, function(err, client) {
+        const db = client.db(dbName);
+        const collection = db.collection('article');
+        const query = { "articleID": articleID };
+        collection.deleteOne(query, function(err, doc) {
+            if (err) {
+                return res.status(404).send('error');
+            } else if (!doc) {
+                return res.status(404).send('article not found');
+            } else
+            //console.log(doc);
+                res.status(200).json(doc);
+        });
+        client.close();
     });
+});
 
-    res.send('Article was deleted');
+app.put('/article/:articleID', (req, res) => {
+    const articleID = req.params.articleID;
+    const article = { $set: req.body };
+
+    MongoClient.connect(url, function(err, client) {
+        const db = client.db(dbName);
+        const collection = db.collection('article');
+        const query = { "articleID": articleID };
+        collection.updateOne(query, article, { upsert: false }, function(err, result) {
+            if (err) throw err
+            res.status(200).json(result);
+        });
+        client.close();
+    });
 });
 
 app.listen(port, () => console.log(`articles API listening on port ${port}!`))
